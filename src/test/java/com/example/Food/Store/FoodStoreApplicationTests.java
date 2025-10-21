99ed51e322c688e766352552156e300b475c25ba
Ezequiel <ezequielw22l@gmail.com>
2025-10-21 11:02:45 -0300

    primer comit

---
+++ src/test/java/com/example/Food/Store/FoodStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Food.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
